a4bef769fd1e37cb733a5e8c0bd4a18a37c73667
Jimmy <jimmyatridhyatech@gmail.com>
2023-03-10 00:59:02 +0530

    Removed Unused apps

--- signupapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'signupapp'
-urlpatterns=[
-    path('login-user',views.login_user,name='loginuser'),
-    path('register-user',views.register_user,name='registeruser'),
-]
